22e7334030f9a195ef769c6a2ee2fea4467d064f
barstars <mtangirkul@gmail.com>
2024-06-06 13:14:42 +0500

    My reume pdf to mp3

--- pdftomp3.py
+++ pdftomp3.py
@@ -2,9 +2,9 @@ from pdfminer.high_level import extract_text
 from gtts import gTTS
 #text = (((extract_text("C:/Users/MUCHASCH/Downloads/IdentityCard-041228551255-20231207130502 (1).pdf").replace("\n",". ")).replace(">",". ")).replace("<",". ")).strip()
 
-text = (extract_text("Exemple.pdf").replace("\n",". ")).strip()
+text = (extract_text('Exemple.pdf').strip())
 
-mp3 = gTTS(text=text, lang="en")
+mp3 = gTTS(text=text, lang="ru")
 mp3.save("BarstarsMP3_Exemple.mp3")
 
 print(text)
